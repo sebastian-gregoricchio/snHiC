ea1d1518649e571e124e3f07db36abf6deb6823f
Sebastian Gregoricchio <52447016+sebastian-gregoricchio@users.noreply.github.com>
2023-06-01 17:22:22 +0200

    Delete install_dcHiC.sh

--- workflow/envs/install_dcHiC.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-$1/bin/git clone https://github.com/ay-lab/dcHiC $1/bin/dcHiC
-$1/bin/R CMD INSTALL $1/bin/dcHiC/packages/functionsdchic_1.0.tar.gz
-
-echo ''
-echo '>>> dcHiC installed in:' $1/bin/dcHiC/ '<<<'
