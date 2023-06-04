9f5dad0103fdc3a999403186035f99e71cde59e6
Sebastian Gregoricchio <52447016+sebastian-gregoricchio@users.noreply.github.com>
2023-06-04 10:06:21 +0200

    Add files via upload

--- workflow/envs/install_dependences.sh
+++ workflow/envs/install_dependences.sh
@@ -9,6 +9,9 @@ $1/bin/git clone https://github.com/akdemirlab/HiCPlotter $1/bin/HiCPlotter
 cp $1/bin/HiCPlotter/HiCPlotter.py $1/bin/
 chmod +x $1/bin/HiCPlotter.py
 
+mv $1/lib/python3.9/site-packages/selfish/selfish.py $1/lib/python3.9/site-packages/selfish/selfish_original.py
+sed 's/import straw/import hicstraw as straw/' $1/lib/python3.9/site-packages/selfish/selfish_original.py > $1/lib/python3.9/site-packages/selfish/selfish.py
+
 echo ''
-echo '>>> dcHiC, GENOVA and HiCPlotter are installed <<<'
-echo '>>> dcHiC folder is avilable at:' $1/bin/dcHiC/ '<<<'
+echo '     >>> SELFISH has been fixed <<<'
+echo '     >>> dcHiC, stripeDiff, GENOVA and HiCPlotter are installed <<<'
